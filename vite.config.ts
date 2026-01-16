b696b8452c3bd785663ded507e067b1aa031c93e
CYBER-THREAT-CONSULTING <31601971+CTC-Kernel@users.noreply.github.com>
2026-01-16 06:08:42 +0100

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
     port: 8080,
     headers: {
       'Cross-Origin-Embedder-Policy': 'unsafe-none',
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups'
+      'Cross-Origin-Opener-Policy': 'unsafe-none'
     },
     proxy: {
       '/api': {
