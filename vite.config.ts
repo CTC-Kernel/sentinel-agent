e8253c0eb40b7f570cde42cf1600d8dd6a696c3c
CYBER-THREAT-CONSULTING <31601971+CTC-Kernel@users.noreply.github.com>
2026-01-22 00:16:27 +0100

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -31,14 +31,14 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/icons/icon-192.webp',
+            src: '/icons/icon-192.png',
             sizes: '192x192',
-            type: 'image/webp'
+            type: 'image/png'
           },
           {
-            src: '/icons/icon-512.webp',
+            src: '/icons/icon-512.png',
             sizes: '512x512',
-            type: 'image/webp'
+            type: 'image/png'
           }
         ]
       },
